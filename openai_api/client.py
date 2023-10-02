63e2659c81d29e7344d8f590cb7e5157543a2e39
ericsondevmaster <ericson.devmaster@gmail.com>
2023-10-02 12:44:45 -0300

    First commit

---
+++ openai_api/client.py
@@ -0,0 +1,17 @@
+import openai
+from django.conf import settings
+
+def get_car_ai_bio(model, brand, year):
+    prompt = ''''
+    Me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. Fale coisas específicas desse modelo.
+    Descreva especificações técnicas desse modelo de carro.
+    '''
+
+    openai.api_key = settings.OPEN_API_KEY
+    prompt = prompt.format(brand, model, year)
+    response = openai.Completion.create(
+        model='text-davinci-003',
+        prompt=prompt,
+        max_tokens=1000,
+    )
+    return response['choices'][0]['text']
